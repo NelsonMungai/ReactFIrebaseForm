42653c5a70b097c72dae553914bf9eb06c781b4f
Nelson Mungai <nelson.irungu@students.jkuat.ac.ke>
2024-01-10 07:16:35 +0300

    created a react app and installed dependencies

---
+++ ContactForm/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import HelloWorld from './hello'
+import Contact from './contact'
+function App() {
+
+  return (
+    <>
+    <Contact /> 
+    </>
+  )
+}
+
+export default App
